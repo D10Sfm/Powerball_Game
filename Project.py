992c6c9bebf91e8b45a4a1e554868e7f5c8b96c4
D10Sfm <fasil.1232@gmail.com>
2022-04-05 19:02:22 +0300

    Nots update!

--- Project.py
+++ Project.py
@@ -2,6 +2,6 @@ from Main import Powerball
 subscription1 = Powerball()  # Create an instance in the game class(Powerball)
 subscription2 = Powerball()  # Create an instance in the game class(Powerball)
 subscription3 = Powerball()  # Create an instance in the game class(Powerball)
-subscription1.powerball()
-subscription2.powerball()
-subscription3.powerball()
+subscription1.powerball()    # Using The Method of the daily draw and win validation
+subscription2.powerball()    # Using The Method of the daily draw and win validation
+subscription3.powerball()    # Using The Method of the daily draw and win validation
