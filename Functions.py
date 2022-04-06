541054e101ae781fafbdf8e7910ba32c35ad51e0
D10Sfm <fasil.1232@gmail.com>
2022-04-06 13:38:44 +0300

    Comments updating!

--- Functions.py
+++ Functions.py
@@ -7,32 +7,32 @@ from colorama import Style    # import Style lib for the style of the color
 def displayed(y, f,):    # def a function for the result of the subscription
     if y == 5:     # y representing the amount of hits of the subscription on the whiteball
         if f:      # f is the flag for the hitting on the powernumber ,f=True=hit on powerball f=False dont hit the powerball
-            print(f"{y} Correct White Balls and Powerball: Jackpot $324,000,000")
+            print(f"{y} Correct White Balls and Powerball: Jackpot"+" "+Fore.YELLOW+"$324,000,000"+Style.RESET_ALL)
         else:
-            print(f"{y} Correct White Balls and no Powerball: $1,000,000")
+            print(f"{y} Correct White Balls and no Powerball:"+" "+Fore.YELLOW+"$1,000,000"+Style.RESET_ALL)
     elif y == 4:
         if f:
-            print(f"{y} Correct White Balls and Powerball: $10,000")
+            print(f"{y} Correct White Balls and Powerball:"+" "+Fore.YELLOW+"$10,000"+Style.RESET_ALL)
         else:
-            print(f"{y} Correct White Balls and no Powerball: $100")
+            print(f"{y} Correct White Balls and no Powerball:"+" "+Fore.YELLOW+"$100"+Style.RESET_ALL)
     elif y == 3:
         if f:
-            print(f"{y} Correct White Balls and Powerball: $100")
+            print(f"{y} Correct White Balls and Powerball:"+" "+Fore.YELLOW+"$100"+Style.RESET_ALL)
         else:
-            print(f"{y} Correct White Balls and no Powerball: $7")
+            print(f"{y} Correct White Balls and no Powerball:"+" "+Fore.YELLOW+"$7"+Style.RESET_ALL)
     elif y == 2:
         if f:
-            print(f"{y} Correct White Balls and Powerball: $7")
+            print(f"{y} Correct White Balls and Powerball:"+" "+Fore.YELLOW+"$7"+Style.RESET_ALL)
         else:
             print(f"{y} Correct White Balls and no Powerball! Please Try again!")
     elif y == 1:
         if f:
-            print(f"{y} Correct White Balls and Powerball: $4")
+            print(f"{y} Correct White Balls and Powerball:"+" "+Fore.YELLOW+"$4"+Style.RESET_ALL)
         else:
             print(f"{y} Correct White Balls and no Powerball! Please Try again!")
     elif y == 0:
         if f:
-            print("No White Balls Just Powerball: $4")
+            print("No White Balls Just Powerball:"+" "+Fore.YELLOW+"$4"+Style.RESET_ALL)
         else:
             print("Try again!")
     elif y > 5:     # An flag for check if the counter loop is correct
--- Project.py
+++ Project.py
@@ -1,13 +1,20 @@
 from Main import Powerball  # Import the main class for using there methods
 
 Eli = Powerball()  # Create an instance in the game class(Powerball)
-Daniel = Powerball()
-Miki = Powerball()
-nitzi = Powerball()
-nitzi.powerball()
+# Daniel = Powerball()
+# Miki = Powerball()
+# nitzi = Powerball()
+# nitzi.powerball()
 # Eli.__setrange__(38,1)   # set the rang of whiteball drawing from 20 to 37
 # Eli.__setrange__(8,2)    # set the rang of whiteball drawing from 10 to 7
 # Eli.powerball()
 # Daniel.powerball()
 # Miki.__setrange__(50, 3)  # Using The Method of setrange for set an new range of drawing in this scanario the output
 # will be stderr
+# tziona = Powerball()
+# tziona.powerball()
+simha = Powerball()
+# aviva = Powerball()
+# simha.powerball()
+Yalem = Powerball()
+Yalem.powerball()
