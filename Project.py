65e5cec68c059959eb3333eb874766d15fb85aba
D10Sfm <fasil.1232@gmail.com>
2022-04-07 17:39:15 +0300

    updating!

--- Project.py
+++ Project.py
@@ -4,10 +4,10 @@ user1 = Powerball()  # Create an instance in the game class(Powerball)
 user2 = Powerball()
 user3 = Powerball()
 user1.powerball()
-user1.__setrange__(38, 1)  # set the rang of whiteball drawing from 20 to 37
-user1.__setrange__(8, 2)  # set the rang of powerball drawing from 10 to 7
-user1.powerball()
-user2.powerball()
-user3.__setrange__(50, 3)  # Using The Method of setrange for set an new range of drawing in this scanario the output
+# user1.__setrange__(38, 1)  # set the rang of whiteball drawing from 20 to 37
+# user1.__setrange__(8, 2)  # set the rang of powerball drawing from 10 to 7
+# user1.powerball()
+# user2.powerball()
+# user3.__setrange__(50, 3)  # Using The Method of setrange for set an new range of drawing in this scanario the output
 # will be stderr
-user3.powerball()  # The range of the drawing numbers will stay 20 for the whiteballs and 10 for the powerball
+# user3.powerball()  # The range of the drawing numbers will stay 20 for the whiteballs and 10 for the powerball
